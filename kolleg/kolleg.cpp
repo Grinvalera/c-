f3cd9aece7ed9e97030e2bd8fb718480100128a9
Grinvalera <56276566+Grinvalera@users.noreply.github.com>
2019-11-20 17:23:43 +0200

    version v1.0

---
+++ kolleg/kolleg.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//class Time {
+//public:
+//	int minutes;
+//	int seconds;
+//	Time() { minutes = 0; seconds = 0; }
+//	Time(int a, int b) { minutes = a; seconds = b; }
+//	void input();
+//	void operator-(int);
+//	void exit();
+//};
+//
+//void Time::input() {
+//	cout << "Enter minutes = ";
+//	cin >> minutes;
+//
+//	cout << "Enter seconds = ";
+//	cin >> seconds;
+//
+//	cout << minutes << ":" << seconds << endl;
+//}
+//
+//void Time::operator-(int seconds_new) {
+//	int minutes_new, nev, lol;
+//	if (seconds_new > seconds)
+//	{
+//		minutes_new = minutes * 60 + seconds;
+//		nev = minutes_new - seconds_new;
+//		lol = nev % 60;
+//		seconds = lol;
+//		minutes -= 1;
+//	}
+//	else
+//	{
+//		seconds = seconds - seconds_new;
+//	}
+//}
+//
+//void Time::exit()
+//{
+//	Time::operator-;
+//	cout << minutes << ":" << seconds << endl;
+//}
+//
+//
+//int main()
+//{
+//	Time obj;
+//	obj.input();
+//
+//	Time obj2;
+//	int seconds_new;
+//	cout << "Enter number = ";
+//	cin >> seconds_new;
+//	obj - seconds_new;
+//	//Time obj3;
+//	obj.exit();
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+int main()
+{
+	int a = 5;
+	int b = 10;
+
+	cout << a + b << endl;
+	
+	return 0;
+}
